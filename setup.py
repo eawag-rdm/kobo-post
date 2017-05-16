4c4ae3df73f25c60846dfa798643c829da9636c1
Harald von Waldow <harald.vonwaldow@eawag.ch>
2017-05-16 15:04:58 +0200

    bumped patch version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='kobopost',
-    version='1.0.0',
+    version='1.0.1',
 
     description="""Package to post-process surveys that were created with kobotoolbox.
     Currently this provides one command line script: mark_skipped.
